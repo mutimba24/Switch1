af4a269aaaca2ecbbaba2e90b7d463f4aef5af79
root <root@localhost>
2024-04-24 13:04:09 +0300

    auto publish 1713953048649

---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int option;
+    printf("Enter Room:");
+    scanf("%d",&option);
+    switch(option)
+    {
+      case 1:
+        printf("Room1 Lights On!!");
+        break;
+        case 2:
+          printf("Room2 Lights On!!");
+          break;
+          case 3:
+              printf("Room3 Lights On!!");
+              break;
+              
+              case 4:
+                printf("Room4 Lights On!!");
+              
+                break;
+                
+                printf("Room4 Lights On!!");
+                case 5:
+                  break;
+                     printf("Room5 Lights On!!");
+                     case 6:
+                       break;
+                       
+                            printf("Room6 Lights On!!");
+                            break;
+                            default:
+                              printf("Invalid option!!!");
+                              break;
+                }
+                
+                return 0;
+                }
+                            
+                     
+                
+              
+        
+      
+    }
+}
